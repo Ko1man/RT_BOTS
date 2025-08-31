eceb767d694aa18e7dfbe23a1027564eccbe13e1
Ko1man <vkot1492@gmail.com>
2025-09-01 01:17:37 +0500

    new bot v 1.1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Telegraf } from 'telegraf';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const bot = app.get<Telegraf>(getBotToken());
+  app.use(bot.webhookCallback('/telegram/WEBHOOK'));
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
