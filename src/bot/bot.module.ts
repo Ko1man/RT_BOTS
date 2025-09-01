2d12cd2a8df05e1e129d1cb9bee4c9271b0711a3
Ko1man <vkot1492@gmail.com>
2025-09-01 12:46:19 +0500

    bot v1.41

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
 import { BotController } from './bot.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [BotController],
-  providers: [BotService],
+  providers: [BotService, PrismaService],
 })
 export class BotModule {}
